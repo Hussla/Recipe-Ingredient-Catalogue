64b854dda88816d78193c02c6747f5af8d499e5c
Hussla <thehhussain@gmail.com>
2025-07-23 00:37:37 +0100

    clean: Remove unused functions from Program.cs
    - Removed all functions that were moved to service classes
    - Eliminated ~500 lines of duplicate code
    - Kept only essential Program.cs functions (Main, HandleUserChoice, etc.)
    - Program.cs now reduced from ~700 to ~320 lines (55% reduction)
    - All functionality preserved through service layer architecture

--- Recipe Ingredient Catalogue/obj/Debug/net8.0/Recipe Ingredient Catalogue.AssemblyInfo.cs
+++ Recipe Ingredient Catalogue/obj/Debug/net8.0/Recipe Ingredient Catalogue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipe Ingredient Catalogue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8d100cf114944dd7fdc3d19f116b2b834d5103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58174f6272db72ee0993a7840dde3cd13a131e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipe Ingredient Catalogue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipe Ingredient Catalogue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
