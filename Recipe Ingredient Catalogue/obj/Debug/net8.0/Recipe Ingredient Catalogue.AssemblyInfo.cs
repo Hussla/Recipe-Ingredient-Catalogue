3ded3e10fd19368520306037e2744d705c0abc10
Hussla <thehhussain@gmail.com>
2025-07-23 02:52:55 +0100

    multi-user support + docs

--- Recipe Ingredient Catalogue/obj/Debug/net8.0/Recipe Ingredient Catalogue.AssemblyInfo.cs
+++ Recipe Ingredient Catalogue/obj/Debug/net8.0/Recipe Ingredient Catalogue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipe Ingredient Catalogue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9911cfbfdea10e91a3fc923fe762d646472a83c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23eac5fda04fecddaf3b6090598f04dd3dc7f48c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipe Ingredient Catalogue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipe Ingredient Catalogue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
